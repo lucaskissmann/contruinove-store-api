66ce9814efb04c4c03bdb9066d8ef64e2869d1fb
Lucas Kissmann <lucaskissmann@gmail.com>
2025-03-22 14:50:59 -0300

    first commit

---
+++ src/main/java/br/com/construinove/store/api/ConstruinoveApplication.java
@@ -0,0 +1,13 @@
+package br.com.construinove.store.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstruinoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstruinoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/construinove/store/api/ConstruinoveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.construinove.store.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstruinoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
